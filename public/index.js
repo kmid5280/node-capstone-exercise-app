ce0f0a1ceadfab557b653eb9b778beac6bd93905
Kyle Middleton <kmiddleton654@gmail.com>
2018-04-12 16:32:25 -0600

    implemented login functionality

--- public/index.js
+++ public/index.js
@@ -1,17 +1,20 @@
 function watchForLogin() {
     $('.login-login-button').on('click', event => {
         event.preventDefault();
+        let token = '';
         const username = $('.login-username-entry').val();
         const password = $('.login-password-entry').val();
         const LOGIN_URL_ENDPOINT = "/auth/login"
         const credentials = {"username": username, "password": password}
         const options = {contentType: "application/json", url: LOGIN_URL_ENDPOINT, data: JSON.stringify(credentials), dataType: "json", method: "POST", processData: false}
         $.ajax(options).done(function(data) {
-            console.log(data)
-            //store token in local storage
-            //redirect to dashboard
-            //do same thing with signup page
+            localStorage.setItem("token", data.authToken)
+            window.location.href = "/dashboard.html"
         })
+        .fail(function() {
+            $('main').append(`Invalid login name or password`)
+        })
+
     })
 }
 
