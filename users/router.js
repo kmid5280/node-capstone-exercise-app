b4bb9c147e802a90f856f7ebeac8a51bbf074900
Kyle Middleton <kmiddleton654@gmail.com>
2018-04-26 15:52:25 -0600

    removed unused code

--- users/router.js
+++ users/router.js
@@ -90,7 +90,7 @@ router.post('/', jsonParser, (req, res) => {
     .count()
     .then(count => {
       if (count > 0) {
-        // There is an existing user with the same username
+        
         return Promise.reject({
           code: 422,
           reason: 'ValidationError',
@@ -98,7 +98,7 @@ router.post('/', jsonParser, (req, res) => {
           location: 'username'
         });
       }
-      // If there is no existing user, hash the password
+      
       return User.hashPassword(password);
     })
     .then(hash => {
@@ -120,10 +120,6 @@ router.post('/', jsonParser, (req, res) => {
     });
 });
 
-// Never expose all your users like below in a prod application
-// we're just doing this so we have a quick way to see
-// if we're creating users. keep in mind, you can also
-// verify this in the Mongo shell.
 router.get('/', (req, res) => {
   return User.find()
     .then(users => res.json(users.map(user => user.serialize())))
